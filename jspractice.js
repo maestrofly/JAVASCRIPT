8cf887de9d5d9bc5e3db595b078dd7c1c260c8b6
maestrofly <derrickalberto@ymail.com>
2023-09-19 16:12:06 +0100

    first commit

---
+++ jspractice.js
@@ -0,0 +1,26 @@
+//variable
+//var, let, const
+
+//var name = 'derrick'; //  global variable
+
+//let nam = "funke";
+
+//console.log(nam);
+
+
+//block
+
+//scope
+//let
+
+function myFunc() {
+    let myVar = "derrick"
+
+    if (1 > 0) {
+        console.log(myVar);
+    }
+     
+    console.log(myVar);
+}
+
+myFunc()
